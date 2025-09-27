d58908748a530f25d40811ab9b0094cf79e093c4
shimak1908-hub <shimak1908@gmail.com>
2025-09-28 01:45:27 +0530

    Delete Band_Name_Generator.py

--- Band_Name_Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-city_name = input("What is the name of the city you grew up on?\n")
-pet_name = input("What is the name of your pet?\n")
-print("Your Band name could be " + (city_name) + " "  + (pet_name))
