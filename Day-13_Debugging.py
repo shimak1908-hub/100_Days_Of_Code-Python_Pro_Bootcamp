85a2de3b82d0cca47ebef5a4571fffb01f64e9ba
shimak1908-hub <shimak1908@gmail.com>
2025-10-19 17:47:38 +0530

    Add files via upload

---
+++ Day-13_Debugging.py
@@ -0,0 +1,36 @@
+#today they gave us 3 codes with bugs so that we can identify the bugs and tackle them
+
+
+
+# def odd_or_even(number):
+#     if number % 2 == 0:
+#         return "This is an even number."
+#     else:
+#         return "This is an odd number."
+#
+#
+# print(odd_or_even(6))
+#
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+#
+# # Target is the number up to which we count
+# def fizz_buzz(target):
+#     for number in range(1, target + 1):
+#         if number % 3 == 0 and number % 5 == 0:
+#             print("FizzBuzz")
+#         elif number % 3 == 0:
+#             print("Fizz")
+#         elif number % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(number)
