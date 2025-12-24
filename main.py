0bc95a98ff8e3872b44067407bdcc9e3de038bb1
shimak1908-hub <shimak1908@gmail.com>
2025-12-25 00:02:56 +0530

    Day-26_NATO

--- main.py
+++ main.py
@@ -1,48 +1,15 @@
-import turtle
-import pandas as pd
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-screen = turtle.Screen()
-screen.title("U.S. States Game")
+import pandas
 
-IMAGE = "blank_states_img.gif"
-screen.addshape(IMAGE)
-turtle.shape(IMAGE)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-data = pd.read_csv("50_states.csv")
-all_states = data.state.to_list()
-guessed_states = []
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(
-        title=f"{len(guessed_states)}/50 States Correct",
-        prompt="Write the name of a U.S. state (or type Exit):"
-    )
-
-    if answer_state is None:
-        continue
-
-    answer_state = answer_state.title()
-
-    if answer_state == "Exit":
-        break
-
-    if answer_state in all_states and answer_state not in guessed_states:
-        guessed_states.append(answer_state)
-
-        state_data = data[data.state == answer_state]
-
-        marker = turtle.Turtle()
-        marker.hideturtle()
-        marker.penup()
-        marker.goto(state_data.x.item(),state_data.y.item())
-        marker.write(answer_state, align="center", font=("Arial", 10, "normal"))
-
-
-missing_states = [state for state in all_states if state not in guessed_states]
-
-pd.DataFrame(missing_states).to_csv("states_to_learn.csv", index=False)
-
-turtle.mainloop()
-
-
-screen.exitonclick()
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
