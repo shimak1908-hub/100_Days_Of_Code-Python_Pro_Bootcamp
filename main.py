0efc61197697505986ce4da08e97b5267091eed2
shimak1908-hub <shimak1908@gmail.com>
2026-02-24 22:43:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+from tkinter import *
 
-import pandas
-
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    kilometer_result_label.config(text=f"{km}")
+window = Tk()
+window.title("Miles to kilometer converter")
+window.config(padx=20 , pady=20)
+miles_input = Entry(width=7)
+miles_input.grid(column=1 , row=0)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2 , row=0)
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0 , row=1)
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1 , row=1)
+kilometer_label = Label(text="km")
+kilometer_label.grid(column=2 , row=1)
+calculate_button = Button(text="Calculate" , command=miles_to_km )
+calculate_button.grid(column=1 , row=2)
+window.mainloop()
